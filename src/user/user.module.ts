eaf5f41b9b3d5e2d82cad1a0e7ad308aef00f9b7
marcelomoreno <marcelo.moreno@avalara.com>
2021-04-12 00:13:45 -0300

    feat: added new endpoint /vi/ranking

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
 
-import { ProgressEntity } from './progress.entity';
+import { ProgressEntity } from '../progress/progress.entity';
 import { UserController } from './user.controller';
 import { UserEntity } from './user.entity';
 import { UserService } from './user.service';
